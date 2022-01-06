c05399e64051fec7f045b0257a310835617bf633
mokshdoshi007 <67779949+mokshdoshi007@users.noreply.github.com>
2022-01-06 18:34:51 +0530

    Delete django directory

--- django/project/studapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from studapp.models import Student
-admin.site.register(Student)
--- django/project/studapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StudappConfig(AppConfig):
-    name = 'studapp'
